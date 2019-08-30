bfe1306202f4fdf6e7dfcf948184e9b06c1f0b8b
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-08-30 13:27:24 -0500

    added condition for nav bar links based on user authentication

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,13 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 
 class NavBar extends Component {
+
+    isAuthenticated = () => sessionStorage.getItem("activeUser") !== null
+
     logOut = (event) => {
         sessionStorage.removeItem("activeUser")
         this.props.history.push("/welcome")
-      }
+    }
 
     render() {
-        return (
+        return this.isAuthenticated()
+        ?(
             <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
                 <ul className="nav nav-pills nav-fill">
                     <li className="nav-item">
@@ -29,6 +33,16 @@ class NavBar extends Component {
                 <button onClick={this.logOut}>Log Out</button>
             </nav>
         )
+        :<nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+                <ul className="nav nav-pills nav-fill">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">Register</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Log In</Link>
+                    </li>
+                </ul>
+            </nav>
     }
 }
 
