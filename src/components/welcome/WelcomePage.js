593000d3d3f0f13197aa8fe176c558b7aa740946
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-09-05 11:29:22 -0500

    Added my-3 class to buttons on welcome page

--- src/components/welcome/WelcomePage.js
+++ src/components/welcome/WelcomePage.js
@@ -8,8 +8,8 @@ class WelcomePage extends Component {
     return (
       <div className="jumbotron">
         <h1 className="display-4">Welcome to Nutshell!</h1>
-        <p><Link className="welcome-link btn btn-primary" to="/register">Click Here to Register</Link></p>
-        <p><Link className="welcome-link btn btn-primary" to="/login">Existing Users Log In Here</Link></p>
+        <p><Link className="welcome-link btn btn-primary my-3" to="/register">Click Here to Register</Link></p>
+        <p><Link className="welcome-link btn btn-primary my-3" to="/login">Existing Users Log In Here</Link></p>
       </div>
     )
   }
